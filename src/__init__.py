3f2123812e21c833abd0c4f416790e14ec71dccc
Shruthi Kashetty <150764514+Shruthik99@users.noreply.github.com>
2025-11-05 15:51:36 -0500

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+"""
+Smart City Energy Analysis Package
+MLOps Lab Enhancement for Northeastern University
+"""
+
+__version__ = "1.0.0"
+__author__ = "Smart City Analytics Team"
+
+# This file makes the src directory a Python package
+# allowing imports like: from src.energy_analysis import load_data
